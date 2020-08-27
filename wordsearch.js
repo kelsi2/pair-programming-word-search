b4da9121a25c7a0aee998600ded72deaca33243e
Kelsi Proulx <Kelsiproulx1919@gmail.com>
2020-08-26 18:06:01 -0700

    Add files via upload

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const transposed = transpose(letters);
+  const matrix = {
+    horizontalJoin: letters.map(ls => ls.join('')),
+    verticalJoin: transposed.map(ls => ls.join('')),
+    backwardsHorizontal: letters.map(ls => ls.reverse().join('')),
+    backwardsVertical: transposed.map(ls => ls.reverse().join(''))
+  };
+  for (searches in matrix) {
+    for (l of matrix[searches]) {
+      if (l.includes(word)) return true;
     }
-}
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let newArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!newArr[j]) {
+        newArr[j] = [];
+      }
+      newArr[j][i] = matrix[i][j];
+    }
+  }
+  return newArr;
+};
+
+module.exports = wordSearch;
